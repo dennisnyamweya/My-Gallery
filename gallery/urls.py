757ee419c6eb0fa8d664d32dd17b60e2e2de66ce
dennisnyamweya <denisnyamweya813@gmail.com>
2019-08-24 17:19:52 +0300

    added url patterns

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+#url patterns
 urlpatterns = [
     url(r'^$', views.picture_list,name='picture_list'),
     url(r'^(?P<category_slug>[-\w]+)/$', views.picture_list,name='picture_list_by_category'),
