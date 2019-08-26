4de976fc13f4f580aeeb5b31ae8998a28efb57b5
dennisnyamweya <denisnyamweya813@gmail.com>
2019-08-26 10:21:33 +0300

    Deployment

--- Procfile
+++
@@ -1,2 +0,0 @@
-
-web: gunicorn gallery.wsgi --log-file -
--- mygallery/settings.py
+++ mygallery/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import django_heroku
-import dj_database_url
-from decouple import config,Csv
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'mygallery.urls'
